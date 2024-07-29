361d016878614b2ac2f746ed6e64ce2644a171b0
User <toleu.era10.2004@gmail.com>
2024-07-29 12:30:53 +0500

    .

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 document.getElementById("timeBtn").addEventListener('click', (event) => {
     showDateTime();    
 });
+document.getElementById("name-group").addEventListener('click', (event) => {
+    showNameGroup();
+});
 
 const graduationDate = new Date("June 15, 2025 00:00:00");
 
@@ -9,8 +12,8 @@ function showDateTime() {
     document.getElementById("year").innerText = now.getFullYear().toLocaleString();
     document.getElementById("dayOfWeek").innerText = dayOfWeek(now.getDate());
     document.getElementById("day").innerText = now.getDate().toLocaleString();
-    document.getElementById("month").innerText = now.getMonth().toLocaleString();
-    document.getElementById("graduate").innerText = ((graduationDate.getTime() - now.getTime()) / 2073600).toLocaleString();
+    document.getElementById("month").innerText = monthInText(now.getMonth());
+    document.getElementById("graduate").innerText = Math.round((graduationDate.getTime() - now.getTime()) / (1000 * 3600 * 24)).toLocaleString() + " days!";
 }
 function dayOfWeek(day){
     switch (day%7){
@@ -30,3 +33,38 @@ function dayOfWeek(day){
             return "Sunday";
     }
 }
+
+function monthInText(month){
+    switch (month % 12){
+        case 0:
+            return "December";
+        case 1:
+            return "January";
+        case 2:
+            return "February";        
+        case 3:
+            return "March";
+        case 4:
+            return "April";
+        case 5:
+            return "May";
+        case 6:
+            return "June";
+        case 7:
+            return "July";
+        case 8:
+            return "August";
+        case 9:
+            return "September";
+        case 10:
+            return "October";
+        case 11:
+            return "November";
+    }
+}
+
+function showNameGroup(){
+    document.getElementById("name").innerText = "Yersayan Toleu";
+    document.getElementById("group").innerText = "SE-2217";
+}
+
